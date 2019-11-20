e547517214142e49e27cba7dc663a8bc705b50ae
Monica  Karasek <mlkarasek@gmail.com>
2019-11-20 20:29:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  tasks.appendChild(listItem);
+  form.reset();
+  });
+
+tasks.addEventListener('click', function(event){
   let listItem = event.target.parentNode;
   if (event.target.className == "exit")
     const taskList = new TaskList();
